33a8a44ec70ae7901e91f0a8f499912e6f1c05d3
eltonnk <ahenri333@gmail.com>
2024-04-19 16:45:32 -0400

    using parameters from previous iteration of the ga to as init params for next run
    also can now plot past runs

---
+++ compare_ga_exp_data.py
@@ -0,0 +1,134 @@
+from typing import Dict
+from time import time
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import pysysid.genetic as genetic
+import pysysid.pm2i as pm2i
+import pysysid.signal_generators as sg
+
+
+def _main():
+
+    SHOW_ANG_VEL = True
+
+    data_path = pathlib.Path("data/2024_04_19")
+
+    data_files = list(pathlib.Path.glob(data_path, "hybrid_2*"))
+
+    df_hybrid = pd.read_csv(data_files[-1], sep=", ")
+
+    df_hybrid = df_hybrid.head(1000)
+
+    t = np.array(df_hybrid["t"])
+
+    v_cmd_m = np.array(df_hybrid["cmd_voltage_motor"])
+    v_cmd_b = np.array(df_hybrid["cmd_voltage_brake"])
+    tau_u = np.array(df_hybrid["torque"])
+
+    i_m = np.array(df_hybrid["current_motor"])
+    i_b = np.array(df_hybrid["current_brake"])
+    theta = np.array(df_hybrid["theta"])
+    omega = np.array(df_hybrid["omega"])
+
+    fig, ax = plt.subplots(3, 2)
+
+    ax[0][0].set_xlabel(r"$t$ (s)")
+    ax[0][0].set_ylabel(r"$v_{m}(t)$ (V)")
+    ax[0][0].plot(t, v_cmd_m, label=r"Motor Voltage Command", color="C0")
+    ax[0][0].legend(loc="upper right")
+
+    ax[1][0].set_xlabel(r"$t$ (s)")
+    ax[1][0].set_ylabel(r"$v_{b}(t)$ (V)")
+    ax[1][0].plot(t, v_cmd_b, label=r"Brake Voltage Command", color="C0")
+    ax[1][0].legend(loc="upper right")
+
+    ax[2][0].set_xlabel(r"$t$ (s)")
+    ax[2][0].set_ylabel(r"$\tau_{d}(t)$ (Nm)")
+    ax[2][0].plot(t, tau_u, label=r"Load Torque", color="C0")
+    ax[2][0].legend(loc="upper right")
+
+    ax[0][1].set_xlabel(r"$t$ (s)")
+    ax[0][1].set_ylabel(r"$i_{m}(t)$ (A)")
+    ax[0][1].plot(t, i_m, label=r"Motor current", color="C0")
+    ax[0][1].legend(loc="upper right")
+
+    ax[1][1].set_xlabel(r"$t$ (s)")
+    ax[1][1].set_ylabel(r"$i_{b}(t)$ (A)")
+    ax[1][1].plot(t, i_b, label=r"Brake Current", color="C0")
+    ax[1][1].legend(loc="upper right")
+
+    if SHOW_ANG_VEL:
+        ax[2][1].set_xlabel(r"$t$ (s)")
+        ax[2][1].set_ylabel(r"$\omega(t)$ (rad/s)")
+        ax[2][1].plot(t, omega, label=r"Angular Velocity", color="C0")
+        ax[2][1].legend(loc="upper right")
+    else:
+        ax[2][1].set_xlabel(r"$t$ (s)")
+        ax[2][1].set_ylabel(r"$\theta(t)$ (rad)")
+        ax[2][1].plot(t, theta, label=r"Angular Position", color="C0")
+        ax[2][1].legend(loc="upper right")
+
+    data_path = pathlib.Path("data/2024_04_19")
+
+    data_files = list(pathlib.Path.glob(data_path, "hybfit_*"))
+
+    df_fit = pd.read_csv(data_files[-1], sep=",")
+
+    df_fit = df_fit.head(1000)
+
+    t = np.array(df_fit["t"])
+
+    v_cmd_m = np.array(df_fit["cmd_voltage_motor"])
+    v_cmd_b = np.array(df_fit["cmd_voltage_brake"])
+    tau_u = np.array(df_fit["torque"])
+
+    i_m = np.array(df_fit["current_motor"])
+    i_b = np.array(df_fit["current_brake"])
+    theta = np.array(df_fit["theta"])
+    omega = np.array(df_fit["omega"])
+
+    ax[0][0].set_xlabel(r"$t$ (s)")
+    ax[0][0].set_ylabel(r"$v_{m}(t)$ (V)")
+    ax[0][0].plot(t, v_cmd_m, label=r"Id. Motor Voltage Command", color="C1")
+    ax[0][0].legend(loc="upper right")
+
+    ax[1][0].set_xlabel(r"$t$ (s)")
+    ax[1][0].set_ylabel(r"$v_{b}(t)$ (V)")
+    ax[1][0].plot(t, v_cmd_b, label=r"Id. Brake Voltage Command", color="C1")
+    ax[1][0].legend(loc="upper right")
+
+    ax[2][0].set_xlabel(r"$t$ (s)")
+    ax[2][0].set_ylabel(r"$\tau_{d}(t)$ (Nm)")
+    ax[2][0].plot(t, tau_u, label=r"Id. Load Torque", color="C1")
+    ax[2][0].legend(loc="upper right")
+
+    ax[0][1].set_xlabel(r"$t$ (s)")
+    ax[0][1].set_ylabel(r"$i_{m}(t)$ (A)")
+    ax[0][1].plot(t, i_m, label=r"Id.  Motor current", color="C1")
+    ax[0][1].legend(loc="upper right")
+
+    ax[1][1].set_xlabel(r"$t$ (s)")
+    ax[1][1].set_ylabel(r"$i_{b}(t)$ (A)")
+    ax[1][1].plot(t, i_b, label=r"Id. Brake Current", color="C1")
+    ax[1][1].legend(loc="upper right")
+
+    if SHOW_ANG_VEL:
+        ax[2][1].set_xlabel(r"$t$ (s)")
+        ax[2][1].set_ylabel(r"$\omega(t)$ (rad/s)")
+        ax[2][1].plot(t, omega, label=r"Id. Angular Velocity", color="C1")
+        ax[2][1].legend(loc="upper right")
+    else:
+        ax[2][1].set_xlabel(r"$t$ (s)")
+        ax[2][1].set_ylabel(r"$\theta(t)$ (rad)")
+        ax[2][1].plot(t, theta, label=r"Id. Angular Position", color="C1")
+        ax[2][1].legend(loc="upper right")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    _main()
--- hybrid_id_real_data.py
+++ hybrid_id_real_data.py
@@ -413,26 +413,48 @@ def _main():
         0.0,
     ]
 
-    initial_estimate_motor_params = {
-        "brake_alpha_0": 0.3,  # multiplied by hysteresis variable z that directly affects the torque, so lets give it a reasonable value
-        "brake_alpha_1": 0.0005,  # same as above, multiplied by the current, so let's keep it lower
-        "brake_c_0": 3e-4,  # lets set the brake's viscous damping to the twice the motor's value
-        "brake_c_1": 3e-2,  # let's assume this is quite high, since the brake should be able to produce high torque with little energy
-        "brake_R": 0.4,  # datasheet b6 (further refined by looking at current curves)
-        "brake_L": 0.06,  # assuming the brake's electrical time constant is 3 times as big as the motor's electrical time constant
-        "brake_A": 1,  # read that this parameter is superfluous and can be set to any values, other values in the hysteresis model wil adapt
-        "brake_gamma": 1e-5,  # TODO: no clue
-        "brake_beta": 1e1,  # TODO : no clue
-        "brake_J": 2.6e-6,  # datasheet b6
-        "motor_K": 0.14132959412,  # datasheet s23
-        "motor_R": 1.18,  # datasheet s23
-        "motor_L": 48e-4,  # taking a guess (further refined by looking at current curves)
-        "motor_B": 1.534e-4,  # since mechanical time constant should be 100 times bigger than electrical time constant, after computation, this is the result
-        "motor_J": 7.8e-6,  # lets guess 3 times the value of the brake
-        "m_driver_K": 10.0,  # total guess
-        "m_driver_w0": 400.0,  # total guess
-        "b_driver_K": 12.0,  # total guess
-        "b_driver_w0": 400.0,  # total guess
+    # initial_estimate_motor_params = {
+    #     "brake_alpha_0": 0.3,  # multiplied by hysteresis variable z that directly affects the torque, so lets give it a reasonable value
+    #     "brake_alpha_1": 0.0005,  # same as above, multiplied by the current, so let's keep it lower
+    #     "brake_c_0": 3e-4,  # lets set the brake's viscous damping to the twice the motor's value
+    #     "brake_c_1": 3e-2,  # let's assume this is quite high, since the brake should be able to produce high torque with little energy
+    #     "brake_R": 0.4,  # datasheet b6 (further refined by looking at current curves)
+    #     "brake_L": 0.06,  # assuming the brake's electrical time constant is 3 times as big as the motor's electrical time constant
+    #     "brake_A": 1,  # read that this parameter is superfluous and can be set to any values, other values in the hysteresis model wil adapt
+    #     "brake_gamma": 1e-5,  # TODO: no clue
+    #     "brake_beta": 1e1,  # TODO : no clue
+    #     "brake_J": 2.6e-6,  # datasheet b6
+    #     "motor_K": 0.14132959412,  # datasheet s23
+    #     "motor_R": 1.18,  # datasheet s23
+    #     "motor_L": 48e-4,  # taking a guess (further refined by looking at current curves)
+    #     "motor_B": 1.534e-4,  # since mechanical time constant should be 100 times bigger than electrical time constant, after computation, this is the result
+    #     "motor_J": 7.8e-6,  # lets guess 3 times the value of the brake
+    #     "m_driver_K": 10.0,  # total guess
+    #     "m_driver_w0": 400.0,  # total guess
+    #     "b_driver_K": 12.0,  # total guess
+    #     "b_driver_w0": 400.0,  # total guess
+    # }
+
+    initial_estimate_motor_params = {  # taken from data/2024/02/17 final results
+        "brake_alpha_0": 1.2339353154072616,
+        "brake_alpha_1": 0.0010693706580100638,
+        "brake_c_0": 0.0004190006356950035,
+        "brake_c_1": 0.0732595810249624,
+        "brake_R": 0.3955930327689322,
+        "brake_L": 0.1068959139843728,
+        "brake_A": 4.610578608300889,
+        "brake_gamma": 1.7227481301816273e-05,
+        "brake_beta": 10.305724441215693,
+        "brake_J": 2.621242268913648e-06,
+        "motor_K": 0.03732217072017984,  # removed minus sign here since K should not in reality be negative
+        "motor_R": 3.4438397496107624,
+        "motor_L": 0.0009122877775185453,
+        "motor_B": 0.00031917518081070364,
+        "motor_J": 1.1813857058653292e-05,
+        "m_driver_K": 29.59490847582887,
+        "m_driver_w0": 1046.5049874823897,
+        "b_driver_K": 30.91307395296483,
+        "b_driver_w0": 619.6931263293621,
     }
 
     ha_pmg = FixedNHAPMG(dt=motor_dt, **initial_estimate_motor_params)
